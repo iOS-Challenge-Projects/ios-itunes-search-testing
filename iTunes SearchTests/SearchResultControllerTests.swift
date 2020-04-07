d413e6322ec8d789b54306f4ed0aa688f9f561cc
fritzgt <tuanis@live.com>
2020-04-07 11:03:14 +0300

    create another test with state and stop measures

--- iTunes SearchTests/SearchResultControllerTests.swift
+++ iTunes SearchTests/SearchResultControllerTests.swift
@@ -30,6 +30,57 @@ class SearchResultControllerTests: XCTestCase {
         //if the fulfill() is not call within the timeout it will fail test
         wait(for: [expectation], timeout: 5)
     }
+    
+    
+    func testSpeedOfTypicalRequest() {
+        
+        measure {
+            let controller = SearchResultController()
+            
+            //1.Create expectation
+            let expectation = self.expectation(description: "Wait for expectations")
+            
+            controller.performSearch(for: "GarageBand", resultType: .software) {
+                //Artificial failure for testing purposes
+                //XCTFail()
+                
+                //3.Let the expectation know that it was fulfill
+                expectation.fulfill()
+            }
+            
+            //2.Timer for expectation to complete
+            //if the fulfill() is not call within the timeout it will fail test
+            wait(for: [expectation], timeout: 2)
+            
+        }
+    }
 
+    func testSpeedOfTypicalRequest2() {
+        
+        measureMetrics([.wallClockTime], automaticallyStartMeasuring: false) {
+            let controller = SearchResultController()
+            
+            //1.Create expectation
+            let expectation = self.expectation(description: "Wait for expectations")
+            
+            //Start measuring
+            self.startMeasuring()
+            
+            controller.performSearch(for: "GarageBand", resultType: .software) {
+                //Artificial failure for testing purposes
+                //XCTFail()
+                //stop measuring
+                self.stopMeasuring()
+                //3.Let the expectation know that it was fulfill
+                expectation.fulfill()
+            }
+            
+            //2.Timer for expectation to complete
+            //if the fulfill() is not call within the timeout it will fail test
+            wait(for: [expectation], timeout: 2)
+            
+        }
+    }
+    
 
 }
